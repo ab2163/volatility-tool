8e68d3aade399884bb45c995fd0dd692d7074ba9
Ajinkya <jinks.b99@gmail.com>
2025-07-19 11:22:47 +0000

    Added sidebar options with bounds

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,28 @@
 import streamlit as st
+import datetime
 
-st.title("🎈 My new streamlit app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("Volatility Tool")
+
+st.sidebar.header('Options')
+
+risk_free_rate = st.sidebar.number_input(
+    'Risk-Free Rate (e.g. 0.025 for 2.5%)',
+    value=0.025,
+    format="%.4f",
+    min_value=0.005,
+    max_value=0.10
+)
+
+y_axis_select = st.sidebar.selectbox(
+    'Select Y-axis:',
+    ('Strike Price', 'Moneyness')
+)
+
+show_data_pts = st.sidebar.checkbox("Overlay Raw Data")
+
+input_date = st.sidebar.date_input(
+    "Select Date", 
+    datetime.date(2023, 3, 31),
+    min_value=datetime.date(2021, 1, 4),
+    max_value=datetime.date(2023, 3, 31)
 )
